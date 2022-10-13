0a95242a60ef6d109efc43fc62c6629ddb40639f
WILHEMS <derekwilhems@gmail.com>
2022-10-13 01:30:54 +0000

    Size Validation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
+
+
 class Square:
-    """Represents a square.
-    Private instance attribute: size.
-    Instantiation with optional size.
-    """
+    '''Class Square object intialized with size
+    '''
+    pass
 
     def __init__(self, size=0):
-        """Initializes the data."""
-        if not isinstance(size, int):
+        '''init method of class Square
+        '''
+        if type(size) != int:
             raise TypeError("size must be an integer")
-        elif size < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
